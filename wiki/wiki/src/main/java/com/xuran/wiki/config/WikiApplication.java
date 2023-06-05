4e7435e21851c014840cdbdc73e57834b55a9132
XRFeng <fxrrrr81@outlook.com>
2023-06-05 14:33:19 +0800

    开发Hello World接口 访问地址：http://127.0.0.1:8880/hello

--- wiki/wiki/src/main/java/com/xuran/wiki/config/WikiApplication.java
+++ wiki/wiki/src/main/java/com/xuran/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.xuran.wiki;
+package com.xuran.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.xuran")
 @SpringBootApplication
 public class WikiApplication {
 
